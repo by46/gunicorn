8791f8b565e83a330b1c0e7eec3b2d632d84bb1a
by46 <ycs_ctbu_2010@126.com>
2016-12-13 15:34:59 +0800

    debug

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+def app(environ, start_response):
+    data = "hello, world\n"
+    status = "200 OK"
+    response_headers = [('Content-Type', 'text/plain'),
+                        ('Content-Length', str(len(data)))]
+    start_response(status, response_headers)
+    return iter([data])
+
+
